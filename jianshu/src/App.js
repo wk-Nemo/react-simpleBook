7d9f6475d4b3da6d005ba836ee3a681408dde3d2
wk-Nemo <2181544173@qq.com>
2021-05-03 00:11:38 +0800

    header

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div className="App">
-     hello world
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './common/header';
+class App extends Component {
+  render () {
+    return (
+      <Header />
+    )
+  }
 }
 
 export default App;
